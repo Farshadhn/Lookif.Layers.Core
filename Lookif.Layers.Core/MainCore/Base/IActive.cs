84b021d86a795b0cfc112a1281e619556e8b9aaa
Farshad HN <sfarshadhn@outlook.com>
2021-11-29 18:52:44 +0330

    IActive is added

---
+++ Lookif.Layers.Core/MainCore/Base/IActive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lookif.Layers.Core.MainCore.Base
+{
+    public  interface IActive
+    {
+        public bool IsActive { get; set; }
+    }
+}
