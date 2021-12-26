84865d1b7eba55828ac20d7c78e056ba57ea66fb
Farshad HN <sfarshadhn@outlook.com>
2021-12-26 17:01:52 +0330

    Empty Ctor for AccessToken class

--- Lookif.Layers.Core/Else/JWT/AccessToken.cs
+++ Lookif.Layers.Core/Else/JWT/AccessToken.cs
@@ -9,7 +9,10 @@ namespace Lookif.Layers.Core.Else.JWT
         public string refresh_token { get; set; }
         public string token_type { get; set; }
         public int expires_in { get; set; }
+        public AccessToken()
+        {
 
+        }
         public AccessToken(JwtSecurityToken securityToken)
         {
             access_token = new JwtSecurityTokenHandler().WriteToken(securityToken);
